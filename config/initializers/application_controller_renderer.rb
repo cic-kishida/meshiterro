aea01c449a5ed333762deca209230246322fdce8
EC2 Default User <ec2-user@ip-172-31-86-30.ec2.internal>
2022-01-10 01:52:34 +0000

    Error in pager implementation

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'e6ed8c006432f9787c6d203929524ee63c4874e537f3aab88229abb63cc72df5ed52b6d95b78c854949b6cd935366c1de74c39537797413e92b1ef3c1ac2e284'
