4ebebccf2dd1e82725140577a1ec0481c569d1a5
EC2 Default User <ec2-user@ip-172-31-86-30.ec2.internal>
2022-01-23 00:14:17 +0000

    completion Ver1.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
 
+#  get 'users/show'  # "rails g controller users show" で重複するので削除
   root 'homes#top'
   devise_for :users
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
-
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
-
   end
+
+  resources :users, only: [:show, :edit, :update]
 
 end
